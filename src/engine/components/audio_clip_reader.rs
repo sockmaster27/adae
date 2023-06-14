e7134e7eb1470bc7dabbf5cc086bc7b4d5abf15d
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2023-06-14 17:21:15 +0200

    Fix resampled audioclip output past end

--- src/engine/components/audio_clip_reader.rs
+++ src/engine/components/audio_clip_reader.rs
@@ -111,7 +111,7 @@ impl AudioClipReader {
     ///
     /// `output` should have exactly two channels.
     ///
-    /// Each channel in `output` should have the length of `range`.
+    /// If the `output` buffers are longer than the range, they will be padded with zeroes.
     fn scale_channels(
         input: &Vec<Vec<Sample>>,
         input_range: Range<usize>,
@@ -124,19 +124,22 @@ impl AudioClipReader {
 
         match input_channels {
             1 => {
-                output[0].copy_from_slice(&input[0][input_range.clone()]);
-                output[1].copy_from_slice(&input[0][input_range]);
+                output[0][..input_range.len()].copy_from_slice(&input[0][input_range.clone()]);
+                output[1][..input_range.len()].copy_from_slice(&input[0][input_range.clone()]);
             }
             2 => {
                 debug_assert_eq!(input[0].len(), input[1].len());
-                output[0].copy_from_slice(&input[0][input_range.clone()]);
-                output[1].copy_from_slice(&input[1][input_range]);
+                output[0][..input_range.len()].copy_from_slice(&input[0][input_range.clone()]);
+                output[1][..input_range.len()].copy_from_slice(&input[1][input_range.clone()]);
             }
             _ => {
                 // This should have been caught while importing
                 panic!("Clip has incompatible number of channels");
             }
         }
+
+        output[0][input_range.len()..].fill(0.0);
+        output[1][input_range.len()..].fill(0.0);
     }
 
     /// Take `input` consisting of a list of exactly 2 channels, and interleave them into the `output`-buffer.
@@ -159,84 +162,107 @@ impl AudioClipReader {
             output[i * CHANNELS + 1] = right;
         }
     }
-}
-impl Source for AudioClipReader {
-    /// Outputs to a buffer of at most the requested size (via the info parameter).
-    /// If the end is reached the returned buffer is smaller than this size.
-    fn output(&mut self, info: &Info) -> &mut [Sample] {
+
+    /// Output with resampling.
+    /// This assumes that `self.resampler` is `Some`.
+    fn output_resampling(&mut self, info: &Info) -> &mut [Sample] {
+        let Info {
+            sample_rate,
+            buffer_size,
+        } = *info;
+
+        let resampler = self
+            .resampler
+            .as_mut()
+            .expect("output_resampling was called on AudioClipReader without resampler");
+
+        let resample_ratio = sample_rate as f64 / self.inner.sample_rate as f64;
+        let resampled_length = self.inner.len() as f64 * resample_ratio;
+        let resampled_pos = self.position as f64 * resample_ratio;
+        let delay = resampler.output_delay() * CHANNELS;
+        let remaining = (resampled_length - resampled_pos).ceil() as usize + delay;
+
+        let output_size = min(buffer_size, remaining);
+
+        let mut filled = 0;
+        while filled < output_size {
+            if self.resample_buffer_unused == 0 {
+                let range = self.position
+                    ..min(
+                        self.position + resampler.input_frames_next(),
+                        self.inner.len(),
+                    );
+                self.position += range.len();
+
+                let mut c = self.channel_scale_buffer.iter_mut();
+                let mut channel_scale_buffer = [
+                    &mut c.next().unwrap()[..resampler.input_frames_next()],
+                    &mut c.next().unwrap()[..resampler.input_frames_next()],
+                ];
+                Self::scale_channels(&self.inner.data, range, &mut channel_scale_buffer);
+
+                let result = resampler.process_into_buffer(
+                    &channel_scale_buffer,
+                    &mut self.resample_buffer,
+                    None,
+                );
+                debug_assert!(result.is_ok(), "Resampler error: {:?}", result);
+                self.resample_buffer_unused = resampler.output_frames_max();
+            }
+
+            let used = resampler.output_frames_max() - self.resample_buffer_unused;
+            let input_range = used..min(used + output_size - filled, resampler.output_frames_max());
+            self.resample_buffer_unused -= input_range.len();
+            let output_range = filled * CHANNELS..(filled + input_range.len()) * CHANNELS;
+
+            filled += input_range.len();
+
+            Self::interleave(
+                &self.resample_buffer,
+                input_range,
+                &mut self.output_buffer[output_range],
+            );
+        }
+
+        &mut self.output_buffer[..output_size * CHANNELS]
+    }
+
+    /// Output without resampling.
+    fn output_not_resampling(&mut self, info: &Info) -> &mut [Sample] {
         let Info {
             sample_rate: _,
             buffer_size,
         } = *info;
 
-        match &mut self.resampler {
-            Some(resampler) => {
-                let output_size = buffer_size;
+        let remaining = self.inner.len() - self.position;
+        let output_size = min(buffer_size, remaining);
 
-                let mut filled = 0;
-                while filled < output_size {
-                    if self.resample_buffer_unused == 0 {
-                        let range = self.position
-                            ..min(
-                                self.position + resampler.input_frames_next(),
-                                self.inner.len(),
-                            );
-                        self.position += range.len();
+        let range = self.position..self.position + output_size;
+        self.position += output_size;
 
-                        let mut c = self.channel_scale_buffer.iter_mut();
-                        let mut channel_scale_buffer = [
-                            &mut c.next().unwrap()[..range.len()],
-                            &mut c.next().unwrap()[..range.len()],
-                        ];
-                        Self::scale_channels(&self.inner.data, range, &mut channel_scale_buffer);
+        let mut c = self.channel_scale_buffer.iter_mut();
+        let mut channel_scale_buffer = [
+            &mut c.next().unwrap()[..output_size],
+            &mut c.next().unwrap()[..output_size],
+        ];
+        Self::scale_channels(&self.inner.data, range, &mut channel_scale_buffer);
+        Self::interleave(
+            &self.channel_scale_buffer,
+            0..output_size,
+            &mut self.output_buffer,
+        );
 
-                        let result = resampler.process_into_buffer(
-                            &channel_scale_buffer,
-                            &mut self.resample_buffer,
-                            None,
-                        );
-                        debug_assert!(result.is_ok(), "Resampler error: {:?}", result);
-                        self.resample_buffer_unused = resampler.output_frames_max();
-                    }
-
-                    let used = resampler.output_frames_max() - self.resample_buffer_unused;
-                    let input_range =
-                        used..min(used + output_size - filled, resampler.output_frames_max());
-                    self.resample_buffer_unused -= input_range.len();
-                    let output_range = filled * CHANNELS..(filled + input_range.len()) * CHANNELS;
-
-                    filled += input_range.len();
-
-                    Self::interleave(
-                        &self.resample_buffer,
-                        input_range,
-                        &mut self.output_buffer[output_range],
-                    );
-                }
-
-                &mut self.output_buffer[..output_size * CHANNELS]
-            }
-            None => {
-                let remaining = self.inner.len() - self.position;
-                let output_size = min(buffer_size, remaining);
-
-                let range = self.position..self.position + output_size;
-                self.position += output_size;
-
-                let mut c = self.channel_scale_buffer.iter_mut();
-                let mut channel_scale_buffer = [
-                    &mut c.next().unwrap()[..output_size],
-                    &mut c.next().unwrap()[..output_size],
-                ];
-                Self::scale_channels(&self.inner.data, range, &mut channel_scale_buffer);
-                Self::interleave(
-                    &self.channel_scale_buffer,
-                    0..output_size,
-                    &mut self.output_buffer,
-                );
-
-                &mut self.output_buffer[..output_size * CHANNELS]
-            }
+        &mut self.output_buffer[..output_size * CHANNELS]
+    }
+}
+impl Source for AudioClipReader {
+    /// Outputs to a buffer of at most the requested size (via the info parameter).
+    /// If the end is reached the returned buffer is smaller than this size.
+    fn output(&mut self, info: &Info) -> &mut [Sample] {
+        if self.resampler.is_some() {
+            self.output_resampling(info)
+        } else {
+            self.output_not_resampling(info)
         }
     }
 }
@@ -272,7 +298,7 @@ mod tests {
 
         for _ in 0..5 {
             let output = acr.output(&Info {
-                sample_rate: 48000,
+                sample_rate: 48_000,
                 buffer_size: 50,
             });
 
@@ -284,7 +310,7 @@ mod tests {
             assert!(-1.001 <= rs && rs <= -0.999, "Sample: {}", rs);
 
             let output = acr.output(&Info {
-                sample_rate: 48000,
+                sample_rate: 48_000,
                 buffer_size: 50,
             });
 
@@ -304,7 +330,7 @@ mod tests {
 
         for _ in 0..5 {
             let output = acr.output(&Info {
-                sample_rate: 48000,
+                sample_rate: 48_000,
                 buffer_size: 50,
             });
 
@@ -323,7 +349,7 @@ mod tests {
 
         for _ in 0..2 {
             let output = acr.output(&Info {
-                sample_rate: 48000,
+                sample_rate: 48_000,
                 buffer_size: 2050,
             });
 
@@ -340,7 +366,7 @@ mod tests {
             assert!(0.999 <= rs && rs <= 1.001);
 
             let output = acr.output(&Info {
-                sample_rate: 48000,
+                sample_rate: 48_000,
                 buffer_size: 2050,
             });
 
@@ -365,7 +391,7 @@ mod tests {
 
         for _ in 0..2 {
             let output = acr.output(&Info {
-                sample_rate: 48000,
+                sample_rate: 48_000,
                 buffer_size: 2050,
             });
 
@@ -383,7 +409,7 @@ mod tests {
         let mut acr = AudioClipReader::new(Arc::new(ac), 1_322_978 + 10, 48_000);
 
         let output = acr.output(&Info {
-            sample_rate: 48000,
+            sample_rate: 48_000,
             buffer_size: 1_322_978 + 10,
         });
 
@@ -396,21 +422,21 @@ mod tests {
 
     #[test]
     fn output_past_end_resampling() {
+        let full_length = (1_322_978.0f64 * (48_000.0 / 44_100.0)).ceil() as usize;
+
         let ac = AudioClip::import(&test_file_path("44100 16-bit.wav")).unwrap();
-        let mut acr = AudioClipReader::new(Arc::new(ac), 1_322_978 + 10, 48_000);
+        let mut acr = AudioClipReader::new(Arc::new(ac), full_length + 10, 48_000);
 
         let output = acr.output(&Info {
-            sample_rate: 48000,
-            buffer_size: 1_322_978 + 10,
+            sample_rate: 48_000,
+            buffer_size: full_length + 10,
         });
 
         assert_eq!(
             output.len(),
-            1_322_978 * CHANNELS,
+            full_length * CHANNELS,
             "Output length does not match"
         );
-
-        dbg!(&output[(1_322_800 * CHANNELS)..]);
     }
 
     #[test]
@@ -419,7 +445,7 @@ mod tests {
         let mut acr = AudioClipReader::new(Arc::new(ac), 50, 48_000);
 
         let output = acr.output(&Info {
-            sample_rate: 48000,
+            sample_rate: 48_000,
             buffer_size: 50,
         });
 
@@ -433,7 +459,7 @@ mod tests {
         acr.jump(0).unwrap();
 
         let output = acr.output(&Info {
-            sample_rate: 48000,
+            sample_rate: 48_000,
             buffer_size: 50,
         });
 
